d0580bf0cf04baf5b90fc0a6560330a652729c65
devnoorfatima <devnoorfatima@gmail.com>
2018-12-16 10:51:17 +0500

    Listed even and odd numbers using while loops also the numbers that are the divisibles of 5 and 3.

--- Javascript-practise/script.js
+++ Javascript-practise/script.js
@@ -1,12 +1,28 @@
+// counting from -19 to 10
 var count = -10;
 while ( count <=19){
     console.log("count is : " + count);
     count++;
 }
 
+// All even numbers between 10 and 40
 var even = 10;
 while ( even <= 40){
 console.log (even);
 even+=2 ;
 }
 
+// All odd numbers betweeen 300 and 333
+var odd = 301;
+while(odd <= 333){
+    console.log(odd);
+    odd+=2;
+}
+
+// Numbers that are divided by both 5 and 3
+var divisibles = 5;
+while (divisibles <=50){
+    if (divisibles % 5 === 0 && divisibles % 3 === 0){
+        console.log(divisibles);
+    }
+}
