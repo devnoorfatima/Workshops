a5a9b304b97bd18691bd10c227f1e5aed806c170
devnoorfatima <devnoorfatima@gmail.com>
2018-12-17 20:59:25 +0500

    Made a function that toggles the color of the background of body.

--- Javascript-practise/objects.js
+++ Javascript-practise/objects.js
@@ -1,20 +1,33 @@
-var movies = [{
-    title : "leap",
-     hasWatched : true,
-      rating : 4.5
-    },
-    {title : "Frozen",
-     hasWatched : false,
-      rating : 5
-    } ]
-    movies.forEach(function(movie){
-      var result = "You have ";
-      if(movie.hasWatched){
-          result += "watched ";
-      }else {
-          result+= "not seen ";
-      }
-      result += movie.title + "-";
-      result += movie.rating + " stars." ;
-        console.log(result);
-    })
+// var movies = [{
+//     title : "leap",
+//      hasWatched : true,
+//       rating : 4.5
+//     },
+//     {title : "Frozen",
+//      hasWatched : false,
+//       rating : 5
+//     } ]
+//     movies.forEach(function(movie){
+//       var result = "You have ";
+//       if(movie.hasWatched){
+//           result += "watched ";
+//       }else {
+//           result+= "not seen ";
+//       }
+//       result += movie.title + "-";
+//       result += movie.rating + " stars." ;
+//         console.log(result);
+//     }
+
+var button = document.querySelector("button");
+var body = document.querySelector("body");
+ var isPurple = false;
+ button.addEventListener("click", function(){
+    if (isPurple){
+        body.style.background="white";
+    }
+    else{
+        body.style.background = "purple";
+    }
+    isPurple=!isPurple;
+ });
