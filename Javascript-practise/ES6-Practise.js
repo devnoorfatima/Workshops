f3c194f1c44949b9c8bf357331542e9de814c30b
devnoorfatima <devnoorfatima@gmail.com>
2018-12-19 22:39:08 +0500

    Practised the lesson of ES6 for everyone.Practised the arrow functions further.

--- Javascript-practise/ES6-Practise.js
+++ Javascript-practise/ES6-Practise.js
@@ -1,47 +1,63 @@
-// Local Variables 
- function setWidth() {
-     var width = 25;
-     console.log(width);
- }
- setWidth();
- console.log(width);
+// // Local Variables 
+//  function setWidth() {
+//      var width = 25;
+//      console.log(width);
+//  }
+//  setWidth();
+//  console.log(width);
 
-// //  Global variables
+// // //  Global variables
 
-var age = 40;
-  if (age > 10) {
- const immunity = age / 4;
- console.log (immunity);
-} 
-console.log(immunity);
+// var age = 40;
+//   if (age > 10) {
+//  const immunity = age / 4;
+//  console.log (immunity);
+// } 
+// console.log(immunity);
 
-// // Let vs Const
+// // // Let vs Const
 
-let points = 550;
-let winner = false;
-if(points > 400){
-let winner = true;
-console.log(winner);
-}
-console.log(winner);
-points = 600;
+// let points = 550;
+// let winner = false;
+// if(points > 400){
+// let winner = true;
+// console.log(winner);
+// }
+// console.log(winner);
+// points = 600;
 
-// Const can't be changed however it's values can be changed for instance:
+// // Const can't be changed however it's values can be changed for instance:
 
-const person = {
-    name : "Noor",
-    age : 17
-} ;
-person.age = 20;  
-// age changed
+// const person = {
+//     name : "Noor",
+//     age : 17
+// } ;
+// person.age = 20;  
+// // age changed
 
-// Arrow Functions =>
-var names = ["max" , "dom" , "fats"];
-const fullName = names.map(function(name){
-return (name + "well");
-});
-// this is an arrow function
-var names = ["max" , "dom" , "fats"];
-const fullName = names.map(name => {
-return (name + "well");
-}); 
+// // Arrow Functions =>
+// var names = ["max" , "dom" , "fats"];
+// const fullName = names.map(function(name){
+// return (name + "well");
+// });
+// // this is an arrow function
+// var names = ["max" , "dom" , "fats"];
+// const fullName = names.map(name => {
+// return (name + "well");
+// }); 
+
+// var age = [22,45,69,88,45,66,100,47];
+// const old = age.filter( age => age >= 60 );
+
+// function calculateBill(total, tax = 0.13, tip = 0.15){
+//     return total + (total * tax ) + (total * tip );
+// }
+// const totalBill = calculateBill (100 , undefined ,0.15);
+// console.log(totalBill);
+
+// const numbers = [3,62,234,7,23,74,23,76,92];
+// const num = numbers.filter (numbers => numbers >= 70);
+// console.log(num);
+
+
+
