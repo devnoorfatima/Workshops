84843d3f2dbf7c590bcfb6bcc99f396f1da11479
devnoorfatima <devnoorfatima@gmail.com>
2018-12-20 12:41:36 +0500

    Practised the arrow functions furthermore, completed the exersice of arrow functions.

--- Javascript-practise/ES6-Practise.js
+++ Javascript-practise/ES6-Practise.js
@@ -59,5 +59,18 @@
 // const num = numbers.filter (numbers => numbers >= 70);
 // console.log(num);
 
+const items =Array.from(document.querySelectorAll("[data-time]"));
+console.log(items);
 
+const filtered = items.filter(item => item.textContent.includes('Flexbox'))
+
+.map(item => item.dataset.time)
+.map(timecode => {
+    const parts = timecode.split(":").map(part => parseFloat(part));
+    return (parts[0] * 60 ) + parts [1];
+}) 
+
+.reduce((totalTime, seconds) => totalTime + seconds,0)
+
+console.log(filtered);
 
