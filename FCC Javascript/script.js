6ad069ae7b060e7ff98f04f7ae4198b0edc96a4f
devnoorfatima <devnoorfatima@gmail.com>
2019-01-07 17:48:54 +0500

    Practised import and export functions of ES6.

--- FCC Javascript/script.js
+++ FCC Javascript/script.js
@@ -1,18 +1,32 @@
-// Mutating const variable:
-const s = [5, 7, 2];
-function editInPlace() {
-  "use strict";
-  // change code below this line
-s[0] = 2;
-s[1] = 5;
-s[2] = 7;
-  }
-editInPlace();
+// // Mutating const variable:
+// const s = [5, 7, 2];
+// function editInPlace() {
+//   "use strict";
+//   // change code below this line
+// s[0] = 2;
+// s[1] = 5;
+// s[2] = 7;
+//   }
+// editInPlace();
 
-// Arrow function with parameter
-const myConcat = (arr1, arr2) => {
-    "use strict";
-    return arr1.concat(arr2);
-  };
-  // test your code
-  console.log(myConcat([1, 2], [3, 4, 5]));
+// // Arrow function with parameter
+// const myConcat = (arr1, arr2) => {
+//     "use strict";
+//     return arr1.concat(arr2);
+//   };
+//   // test your code
+//   console.log(myConcat([1, 2], [3, 4, 5]));
+
+//   Import Function
+  "use strict";
+import {capitalizeString} from "string_functions"
+capitalizeString("hello!");
+
+// Exporting variables
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
+
+// Import the whole data from a file
+"use strict";
+import * as capitalizeStrings from "capitalize_strings";
