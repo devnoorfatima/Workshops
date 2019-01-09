d306410a6f9ee8c50763006adc7ecfe368f390f2
devnoorfatima <devnoorfatima@gmail.com>
2019-01-09 17:01:21 +0500

    Completed the part of basic data structures from free code camp.

--- FCC Javascript/regex.js
+++ FCC Javascript/regex.js
@@ -88,67 +88,142 @@
 
 // for white space \s is used
 // To match non-white space \S is used
-let sample = "Whitespace is important in separating words";
-let countNonWhiteSpace = /\S/ig; 
-let result = sample.match(countNonWhiteSpace);
+// let sample = "Whitespace is important in separating words";
+// let countNonWhiteSpace = /\S/ig; 
+// let result = sample.match(countNonWhiteSpace);
 
-// Check for a doubted favWord(as in color and colour)
-let favWord = "favorite";
-let favRegex = /favou?rite/; 
-let result = favRegex.test(favWord);
+// // Check for a doubted favWord(as in color and colour)
+// let favWord = "favorite";
+// let favRegex = /favou?rite/; 
+// let result = favRegex.test(favWord);
 
-// DEBUGGING
-// Open your browser console
-let outputTwo = "This will print to the browser console 2 times";
-console.log(outputTwo);
-let outputOne = "Try to get this to log only once to the browser console";
-console.clear();
-console.log(outputOne);
+// // DEBUGGING
+// // Open your browser console
+// let outputTwo = "This will print to the browser console 2 times";
+// console.log(outputTwo);
+// let outputOne = "Try to get this to log only once to the browser console";
+// console.clear();
+// console.log(outputOne);
 
 
-// Checking type of variables
-let seven = 7;
-let three = "3";
-console.log(seven + three);
-// Add your code below this line
-console.log(typeof seven);
-console.log(typeof three);
+// // Checking type of variables
+// let seven = 7;
+// let three = "3";
+// console.log(seven + three);
+// // Add your code below this line
+// console.log(typeof seven);
+// console.log(typeof three);
 
-// ========>>> Basic Data structures
-let yourArray = ["hello", 0, true, false, "hurrah!"];
+// // ========>>> Basic Data structures
+// let yourArray = ["hello", 0, true, false, "hurrah!"];
 
-// pop and shift
-function popShift(arr) {
-    let popped = arr.pop();
-    let shifted = arr.shift();
-    return [shifted, popped];
+// // pop and shift
+// function popShift(arr) {
+//     let popped = arr.pop();
+//     let shifted = arr.shift();
+//     return [shifted, popped];
+//   }
+//   console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+// //   splice function
+//   function sumOfTen(arr) {
+//     arr.splice(1,2);
+//     return arr.reduce((a, b) => a + b);
+//   }
+//   console.log(sumOfTen([2, 5, 1, 5, 2, 1]));
+
+// //   Using splice in place of unshift
+//   function htmlColorNames(arr) {
+//     arr.splice(0,2 ,"DarkSalmon","BlanchedAlmond");
+//     return arr;
+//   } 
+//   console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurqoise', 'FireBrick']));
+
+// //   slice function
+//   function forecast(arr) { 
+//     return arr.slice(2,4);
+//   }
+//   console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+// //   Spread function
+//   function spreadOut() {
+//     let fragment = ['to', 'code'];
+//     let sentence= ['learning', ...fragment,'is','fun'] ;
+//     return sentence;
+//   }
+//   console.log(spreadOut());
+
+//   Nested Arrays
+  let myNestedArray = [
+    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+    ['loop', 'shift', 6, 7, 1000, 'method'],
+    ['concat', false, true, 'spread', 'array',["deep"]],
+    ['mutate', 1327.98, 'splice', 'slice', 'push', [["deeper"]]],
+    ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth', [[["deepest"]]] ]
+  ];
+
+//  Adding new key value pairs to a javascript object
+  let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+  };
+  foods ['bananas'] = 13;
+  foods ['grapes'] = 35;
+  foods ['strawberries'] = 27;
+  console.log(foods);
+
+
+//   Nested objects
+  let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+      totalUsers: 51,
+      online: 42
+    }
+  };
+  userActivity.data.online = 45;
+  console.log(userActivity);
+
+//   Delete items
+  let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  delete foods.oranges;
+  delete foods.plums;
+  delete foods.strawberries;
+  console.log(foods);
+
+
+//   hasOwnProperty function
+  let users = {
+    Alan: {
+      age: 27,
+      online: true
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: true
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function isEveryoneHere(obj) {
+  if(users.hasOwnProperty( "Alan", "Jeff", "Sarah","Ryan")){
+    return true
+  }return false;
   }
-  console.log(popShift(['challenge', 'is', 'not', 'complete']));
-
-//   splice function
-  function sumOfTen(arr) {
-    arr.splice(1,2);
-    return arr.reduce((a, b) => a + b);
-  }
-  console.log(sumOfTen([2, 5, 1, 5, 2, 1]));
-
-//   Using splice in place of unshift
-  function htmlColorNames(arr) {
-    arr.splice(0,2 ,"DarkSalmon","BlanchedAlmond");
-    return arr;
-  } 
-  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurqoise', 'FireBrick']));
-
-//   slice function
-  function forecast(arr) { 
-    return arr.slice(2,4);
-  }
-  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
-
-//   Spread function
-  function spreadOut() {
-    let fragment = ['to', 'code'];
-    let sentence= ['learning', ...fragment,'is','fun'] ;
-    return sentence;
-  }
-  console.log(spreadOut());
+  console.log(isEveryoneHere(users));
