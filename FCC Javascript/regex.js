4d420ea21d3264eea13efb96932d95a4e0c8b7d6
devnoorfatima <devnoorfatima@gmail.com>
2019-01-13 23:58:21 +0500

    Practising object oriented programming from free code camp.

--- FCC Javascript/regex.js
+++ FCC Javascript/regex.js
@@ -371,38 +371,108 @@
 
 
 //  Object-oriented-Programming:
-function Dog() {
-  this.name = "Rupert";
-  this.color = "brown";
-  this.numLegs = 4;
-}
-let hound = new Dog();
+// function Dog() {
+//   this.name = "Rupert";
+//   this.color = "brown";
+//   this.numLegs = 4;
+// }
+// let hound = new Dog();
 
 
-function Dog(name,color) {
+// function Dog(name,color) {
+//   this.name = name;
+//   this.color = color;
+//   this.numLegs = 4;
+// }
+// let terrier = new Dog("spot","black");
+
+// function House(numBedrooms) {
+//   this.numBedrooms = numBedrooms;
+// }
+// let myHouse = new House(5);
+// myHouse instanceof House;
+
+
+// function Bird(name) {
+//   this.name = name;
+//   this.numLegs = 2;
+// }
+
+// let canary = new Bird("Tweety");
+// let ownProps = [];
+// for (let property in canary) {
+//   if(canary.hasOwnProperty(property)) {
+//     ownProps.push(property);
+//   }
+// }
+// console.log(ownProps);
+
+function Dog(name) {
   this.name = name;
-  this.color = color;
-  this.numLegs = 4;
 }
-let terrier = new Dog("spot","black");
+Dog.prototype.numLegs = 4;
 
-function House(numBedrooms) {
-  this.numBedrooms = numBedrooms;
-}
-let myHouse = new House(5);
-myHouse instanceof House;
+let beagle = new Dog("Snoopy");
 
-
-function Bird(name) {
+function Dog(name) {
   this.name = name;
-  this.numLegs = 2;
 }
 
-let canary = new Bird("Tweety");
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
 let ownProps = [];
-for (let property in canary) {
-  if(canary.hasOwnProperty(property)) {
+let prototypeProps = []; 
+for (let property in beagle) {
+  if(beagle.hasOwnProperty(property)) {
     ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
   }
 }
-console.log(ownProps);
+
+
+function Dog(name) {
+  this.name = name;
+}
+function joinDogFraternity(candidate) {
+   if (candidate.constructor === Dog) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
+function Dog(name) {
+  this.name = name; 
+}
+
+Dog.prototype = {
+   numLegs: 4, 
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
+
+
+function Dog(name) {
+  this.name = name;
+}
+let beagle = new Dog("Snoopy");
+Dog.prototype.isPrototypeOf(beagle);
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // => true
+Object.prototype.isPrototypeOf(Dog.prototype);
+
