b7e48502aeb457013427e9d6f0f4b98aa1987bb1
Noor Fatima <devnoorfatima@gmail.com>
2019-03-11 11:53:15 +0500

    Making first PR so that my code could be reviewd.

---
+++ calculators/script.js
@@ -0,0 +1,10 @@
+let bill = document.getElementById("bill").value;
+let percentage = document.getElementById("tipPercentage").value;
+let people = document.getElementById("people").value;
+let tipCalculated = document.getElementById("tipCalculated");
+let totalBill = document.getElementById("totalBill");
+let person = document.getElementById("person");
+
+function calculateTip() {
+    
+};
