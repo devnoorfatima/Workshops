72279dd4d3dc5aa8dfb0b75d456bdf3ff3209a0f
Noor Fatima <devnoorfatima@gmail.com>
2019-03-11 16:14:30 +0500

    Added some new styling code and changed the one already present.

--- calculators/script.js
+++ calculators/script.js
@@ -1,10 +1,26 @@
-let bill = document.getElementById("bill").value;
-let percentage = document.getElementById("tipPercentage").value;
-let people = document.getElementById("people").value;
+let bill = document.getElementById("bill");
+let percentage = document.getElementById("tipPercentage");
+let people = document.getElementById("people");
 let tipCalculated = document.getElementById("tipCalculated");
 let totalBill = document.getElementById("totalBill");
 let person = document.getElementById("person");
 
+if (people.value <= 1 || people.value == '') {
+    people.value = 1
+};
+
 function calculateTip() {
-    
+    if (bill.value && percentage.value) {
+        // console.log(bill.value);
+        // console.log(percentage.value);
+        let tip = ((bill.value / 100) * percentage.value);
+        // console.log(tip);
+        tipCalculated.value = tip;
+        //  console.log(tipCalculated);
+        totalBill.value = parseInt(tip) + parseInt(bill.value);
+        person.value =  parseInt(totalBill.value) / parseInt(people.value);
+    } else {
+        alert('Enter the data in fields');
+    }
+
 };
